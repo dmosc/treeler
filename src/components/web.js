2297798697ab1c81045ad86e8b55110e00918870
jorgeabdo <a01194743@itesm.mx>
2019-11-02 17:11:27 -0500

    prueba

---
+++ src/components/web.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Redirect } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
+import Sidebar from "react-sidebar";
+import IMG from "../img/img.jpg"
+
+class Web extends Component {
+    constructor() {
+        super();
+        this.state = {
+
+        }
+    }
+    render() {
+        return (
+            <div>
+            </div>
+        )
+    }
+}
+
+export default Web
