7e64ad6b274b604cd398348c09d03d7111a24525
oscardavidrm <oscar@blockchainacademy.mx>
2019-11-03 06:28:40 -0600

    Tree ENUMS

--- server/database/enums/trees.js
+++ server/database/enums/trees.js
@@ -1,3 +1,34 @@
-const trees = ['PINE', 'COTTONWOOD'];
+const trees = [
+  'Dawn_redwood',
+  'Maidenhair',
+  'Shagbark_hickory',
+  'Sugarberry',
+  'White_oak',
+  'Spindle_Bush',
+  'White_Wax',
+  'Winter_Flowering_Cherry',
+  'Butterfly_Bush',
+  'Mahonia_japonica',
+  'soft_maple',
+  'honey_locust',
+  'cottonwood',
+  'willow',
+  'Douglas_fir',
+  'Heather',
+  'Lantern',
+  'Witch_Hazel',
+  'Camellia',
+  'Rhododendron',
+  'Lavender',
+  'Artemisia',
+  'Rosemary',
+  'Sedum',
+  'Annuals',
+  'weeping_willow',
+  'bald_cypress',
+  'red_twig_dogwood',
+  'river_birch',
+  'red_chokeberry'
+];
 
 module.exports = trees;
